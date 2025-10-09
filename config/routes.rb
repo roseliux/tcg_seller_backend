c6a1b5634858f979a6a59a2aaab69c3264dc0d7d
Rocela Durazo <roxdurazo@gmail.com>
2025-10-08 19:15:12 -0700

    Add CategoriesController and update routes for categories index

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
+  # API health check endpoint
+  get "health" => "application#health"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post "sign_in", to: "sessions#create"
   delete "sign_out", to: "sessions#destroy_current"
   post "sign_up", to: "registrations#create"
   get "me", to: "sessions#current"
+
   resources :sessions, only: [:index, :show, :destroy]
+
   resource  :password, only: [:edit, :update]
   namespace :identity do
     resource :email,              only: [:edit, :update]
     resource :email_verification, only: [:show, :create]
     resource :password_reset,     only: [:new, :edit, :create, :update]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # API health check endpoint
-  get "health" => "application#health"
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :categories, only: :index
 end
